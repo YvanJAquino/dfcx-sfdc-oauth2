c8062c2c8b6f8ac6d11eab8f4940965d8a8f97c0
Yvan J. Aquino <yvan.aquino@cloud-colosseum.net>
2022-03-07 19:52:26 +0000

    update to scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-docker build -t gcr.io/holy-diver-297719/sfdc-oauth2 . 
-docker push gcr.io/holy-diver-297719/sfdc-oauth2
+docker build -t gcr.io/$DEVSHELL_PROJECT_ID/sfdc-oauth2 . 
+docker push gcr.io/$DEVSHELL_PROJECT_ID/sfdc-oauth2
 gcloud alpha run deploy sfdc-oauth2 \
-    --project holy-diver-297719 \
-    --set-env-vars PROJECT_ID=holy-diver-297719 \
+    --project $DEVSHELL_PROJECT_ID \
+    --set-env-vars PROJECT_ID=$DEVSHELL_PROJECT_ID \
     --set-env-vars ENVIRONMENT=PRODUCTION \
-    --image gcr.io/holy-diver-297719/sfdc-oauth2 \
+    --image gcr.io/$DEVSHELL_PROJECT_ID/sfdc-oauth2 \
     --timeout 5m \
     --no-cpu-throttling \
-    --region us-east4 \
+    --region us-central1 \
     --platform managed \
     --min-instances 0 \
     --max-instances 5 \
+    --vpc-connector svpca-sandbox-cloud-run \
     --allow-unauthenticated
 
 
